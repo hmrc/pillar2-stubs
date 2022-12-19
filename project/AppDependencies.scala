d88a1958dc44966e5b1896338397aa8521818051
Akhilesh Kumar <akhilesh.kumar@digital.hmrc.gov.uk>
2022-12-19 11:35:22 +0000

    inital setup

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -1,6 +1,7 @@
 import play.core.PlayVersion
 import play.sbt.PlayImport._
 import sbt.Keys.libraryDependencies
+import play.core.PlayVersion.current
 import sbt._
 
 object AppDependencies {
@@ -14,6 +15,12 @@ object AppDependencies {
 
   val test = Seq(
     "uk.gov.hmrc"             %% "bootstrap-test-play-28"     % bootstrapVersion            % "test, it",
-    
+    "org.mockito"            % "mockito-core"            % "3.7.7"   % "test, it",
+    "com.typesafe.play"      %% "play-test"              % current   % "test, it",
+    "org.pegdown"            % "pegdown"                 % "1.6.0"   % "test, it",
+    "com.vladsch.flexmark"   % "flexmark-all"            % "0.35.10" % "test, it",
+    "org.scalatestplus.play" %% "scalatestplus-play"     % "5.1.0"   % "test, it",
+    "org.scalatestplus"      %% "mockito-3-4"            % "3.2.7.0" % "test",
+    "com.github.tomakehurst" % "wiremock-standalone"     % "2.27.2"  % "test, it"
   )
 }
