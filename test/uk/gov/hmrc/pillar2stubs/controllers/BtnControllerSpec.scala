ace6ef10beaf54356288bb5f1ff9e4582fe75b04
Richard Dowsett <180321016+ridow-hmrc@users.noreply.github.com>
2024-12-03 16:27:09 +0000

    [PIL-1424-btn-submit] linting

--- test/uk/gov/hmrc/pillar2stubs/controllers/BtnControllerSpec.scala
+++ test/uk/gov/hmrc/pillar2stubs/controllers/BtnControllerSpec.scala
@@ -1,22 +1,38 @@
+/*
+ * Copyright 2024 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package uk.gov.hmrc.pillar2stubs.controllers
 
 import org.scalatest.OptionValues
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
 import org.scalatestplus.play.guice.GuiceOneAppPerSuite
+import play.api.libs.json._
 import play.api.mvc.Headers
 import play.api.test.FakeRequest
 import play.api.test.Helpers._
 import uk.gov.hmrc.http.HeaderNames
-import uk.gov.hmrc.pillar2stubs.models.btn.{BTNRequest, BtnErrorResponse, BtnFailureResponsePayload, BtnSuccessResponsePayload}
-import play.api.libs.json._
+import uk.gov.hmrc.pillar2stubs.models.btn._
 
 import java.time.LocalDate
 import scala.util.Random
 
 class BtnControllerSpec extends AnyFunSuite with Matchers with GuiceOneAppPerSuite with OptionValues {
 
-  val validHeaders = (ETMPHeaderFilter.mandatoryHeaders ++ List(HeaderNames.authorisation)).map(_ -> Random.nextString(10))
+  val validHeaders: List[(String, String)] = (ETMPHeaderFilter.mandatoryHeaders ++ List(HeaderNames.authorisation)).map(_ -> Random.nextString(10))
 
   test("Valid BTN submission") {
     val request = FakeRequest(POST, routes.BTNController.submitBTN("XMPLR00000000012").url)
@@ -50,7 +66,5 @@ class BtnControllerSpec extends AnyFunSuite with Matchers with GuiceOneAppPerSui
     status(result) shouldEqual 500
     contentAsJson(result).validate[BtnErrorResponse].asEither.isRight shouldBe true
   }
-
-
 
 }
