d070b54eaea15efd63b9ebf26de9b9d11113d146
joshua-lamptey <220714391+joshua-lamptey@users.noreply.github.com>
2026-07-08 15:29:19 +0100

    [PIL-3050] - update dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -7,12 +7,12 @@ object AppDependencies {
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"   %% "bootstrap-backend-play-30" % bootstrapVersion,
     "org.typelevel" %% "cats-core"                 % "2.13.0",
-    "com.beachape"  %% "enumeratum-play-json"      % "1.8.2"
+    "com.beachape"  %% "enumeratum-play-json"      % "1.9.8"
   )
 
   val test: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"       %% "bootstrap-test-play-30" % bootstrapVersion,
-    "org.mockito"        % "mockito-core"           % "5.16.1",
+    "org.mockito"        % "mockito-core"           % "5.23.0",
     "org.scalatestplus" %% "mockito-4-11"           % "3.2.18.0"
   ).map(_ % Test)
 
